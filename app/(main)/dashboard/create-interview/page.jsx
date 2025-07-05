60c1185adc4c4619c06ba955155676276f5d666e
Shuvkant <weird426344@gmail.com>
2025-07-05 23:52:08 +0545

    creating new interview

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -1,15 +1,25 @@
 "use client"
+import { Progress } from "@/components/ui/progress";
 import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/router";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import FormContainer from "./_components/FormContainer"
 
 function CreateInterview() {
+  const router = useRouter()
+  const [step, setStep] = useState(1)
+  const [formData, setFormData] = useState()
+  const onHandleInputChange = (field, value) => {
+    setFormData((prev) => ({ ...prev, [field]: value }))
+  }
   return (
-    <div>
+    <div className=" px-10 md:px-15 lg:px-44 xl:px-56">
       <div className="flex gap-5 items-center  ">
-        <ArrowLeft className="" />
-        <h2 className="font-bold">Create New Interview</h2>
+        <ArrowLeft onClick={() => router.back()} className="cursor-pointer text-primary" />
+        <h2 className="font-bold text-2xl">Create New Interview</h2>
       </div>
+      <Progress value={step * 33} className="my-5" />
+      <FormContainer />
     </div>
   )
 }
